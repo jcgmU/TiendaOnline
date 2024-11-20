53abf74291f43c2d275a644fa87e24351850d7d0
Juan Camilo García Martín <juancamilogarcia@ucompensar.edu.co>
2024-11-20 10:16:03 -0500

    Update EpaycoConfig.kt

--- app/src/main/java/com/jcgmu/tiendaonline/EpaycoConfig.kt
+++ app/src/main/java/com/jcgmu/tiendaonline/EpaycoConfig.kt
@@ -2,7 +2,7 @@
 package com.jcgmu.tiendaonline
 
 object EpaycoConfig {
-    const val PUBLIC_KEY = "PUBLIC APY KEY"   // Reemplaza con tu Public Key de ePayco
+    const val PUBLIC_KEY = "PUBLIC API KEY"   // Reemplaza con tu Public Key de ePayco
     const val PRIVATE_KEY = "PRIVATE_KEY" // Reemplaza con tu Private Key de ePayco
     const val LANG = "ES" // Idioma: ES o EN
     const val TEST = true // Cambia a false en producción
